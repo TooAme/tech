710e5058e33abc68ba9dfefaaf9c773ded913b8f
Mecyan <14328140+mecyan@user.noreply.gitee.com>
2025-02-28 17:18:00 +0800

    Site updated: 2025-02-28 17:17:59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1740727852888";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1740734275965";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
